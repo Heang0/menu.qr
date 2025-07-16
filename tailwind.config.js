039d3658d4c86e8c2ac306c7d40e1d856d5341d4
Emma Heang <kimheak868@gmail.com>
2025-07-16 17:05:50 +0700

    Update code

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,8 @@ module.exports = {
     // If you add any JS files where you dynamically generate class names, add them here too
     "./frontend/**/*.js",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        // Define your custom font families
-        // 'sans' will be your primary English font (Inter)
-        sans: ['Inter', 'sans-serif'],
-        // 'khmer' specifically for Battambang font
-        khmer: ['Battambang', 'serif'], // Battambang is a serif-style font
-      },
-    },
-  },
+theme: {
+  extend: {},
+},
   plugins: [],
 }
